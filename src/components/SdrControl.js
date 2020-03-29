09b11b71dbaf25dc406ef2c273731b4477ac11bd
Dennis Dunn <ansofive@gmail.com>
2020-03-28 21:33:49 -0700

    OK, let's see if we can control the radio.

--- src/components/SdrControl.js
+++ src/components/SdrControl.js
@@ -1,4 +1,17 @@
-import { Button, Card, CardActionArea, CardActions, CardContent, FormControl, FormControlLabel, makeStyles, Radio, RadioGroup, Switch, TextField, Typography } from "@material-ui/core";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  FormControl,
+  FormControlLabel,
+  makeStyles,
+  Radio,
+  RadioGroup,
+  Slider,
+  Switch,
+  Typography
+} from "@material-ui/core";
 import React, { useEffect, useState } from "react";
 import { callApi } from "./services";
 
@@ -6,12 +19,16 @@ const useStyles = makeStyles(theme => ({
   root: { maxWidth: "40ch" }
 }));
 
+const gainDisplay = v => {
+  return v === 0 ? "Auto" : `${v}`;
+};
+
 export const SdrControl = ({ source, onApply }) => {
   const classes = useStyles();
-  const [freq, setFreq] = useState("");
+  const [freq, setFreq] = useState(88);
   const [mode, setMode] = useState(0);
-  const [gain, setGain] = useState("0");
-  const [squelch, setSquelch] = useState("0");
+  const [gain, setGain] = useState(0);
+  const [squelch, setSquelch] = useState(0);
   const [agc, setAgc] = useState(0);
 
   useEffect(() => {
@@ -27,82 +44,93 @@ export const SdrControl = ({ source, onApply }) => {
 
   return (
     <Card className={classes.root}>
-      <CardActionArea>
-        <CardContent>
-          <Typography variant="h6">Tuner</Typography>
-          <FormControl>
-            <RadioGroup
-              row
-              value={mode}
-              name="mode"
-              onChange={e => {
-                setMode(Number.parseFloat(e.target.value));
-              }}
-            >
-              <FormControlLabel
-                value={0}
-                control={<Radio />}
-                label="FM"
-                labelPlacement="top"
-              />
-              <FormControlLabel
-                value={1}
-                control={<Radio />}
-                label="AM"
-                labelPlacement="top"
-              />
-              <FormControlLabel
-                value={2}
-                control={<Radio />}
-                label="LSB"
-                labelPlacement="top"
-              />
-              <FormControlLabel
-                value={3}
-                control={<Radio />}
-                label="USB"
-                labelPlacement="top"
-              />
-            </RadioGroup>
-            <TextField
-              name="freq"
-              value={freq}
-              label="Frequency MHz"
-              onChange={e => {
-                setFreq(e.target.value);
-              }}
-            />
-            <TextField
-              name="squelch"
-              value={squelch}
-              label="Squelch"
-              onChange={e => {
-                setSquelch(e.target.value);
-              }}
-            />
-            <TextField
-              name="gain"
-              value={gain === "0" ? "auto" : gain}
-              label="Gain"
-              onChange={e => {
-                setGain(e.target.value);
-              }}
+      <CardContent>
+        <FormControl>
+          <RadioGroup
+            row
+            value={mode}
+            onChange={e => {
+              setMode(Number.parseFloat(e.target.value));
+            }}
+          >
+            <FormControlLabel
+              value={0}
+              control={<Radio />}
+              label="FM"
+              labelPlacement="top"
             />
             <FormControlLabel
-              label="AGC"
-              control={
-                <Switch
-                  name="agc"
-                  checked={agc === 1}
-                  onChange={e => {
-                    setAgc(e.target.checked ? 1 : 0);
-                  }}
-                />
-              }
+              value={1}
+              control={<Radio />}
+              label="AM"
+              labelPlacement="top"
             />
-          </FormControl>
-        </CardContent>
-      </CardActionArea>
+            <FormControlLabel
+              value={2}
+              control={<Radio />}
+              label="LSB"
+              labelPlacement="top"
+            />
+            <FormControlLabel
+              value={3}
+              control={<Radio />}
+              label="USB"
+              labelPlacement="top"
+            />
+          </RadioGroup>
+          <Typography id="fLabel">{freq} MHz</Typography>
+          <Slider
+            value={freq}
+            aria-labelledby="gLabel"
+            valueLabelDisplay="off"
+            step={0.1}
+            min={88}
+            max={150}
+            onChange={(e, v) => {
+              setFreq(v);
+            }}
+          />
+          <Typography id="sLabel">Squelch</Typography>
+          <Slider
+            value={squelch}
+            aria-labelledby="sLabel"
+            valueLabelDisplay="auto"
+            step={1}
+            min={0}
+            max={10}
+            marks
+            onChange={(e, v) => {
+              setSquelch(v);
+            }}
+          />
+          <Typography id="gLabel">Gain</Typography>
+          <Slider
+            value={gain}
+            aria-labelledby="gLabel"
+            valueLabelDisplay="auto"
+            valueLabelFormat={gainDisplay}
+            step={0.1}
+            min={0}
+            max={50}
+            onChange={(e, v) => {
+              setGain(v);
+            }}
+          />
+          <FormControlLabel
+            label="AGC"
+            labelPlacement="start"
+            control={
+              <Switch
+                name="agc"
+                checked={agc === 1}
+                onChange={e => {
+                  setAgc(e.target.checked ? 1 : 0);
+                }}
+              />
+            }
+          />
+        </FormControl>
+      </CardContent>
       <CardActions>
         <Button
           onClick={() => onApply({ mode, freq, squelch, gain, agc })}
